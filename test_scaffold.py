3c1bef0ef2a8eedec5fc9eb9d92c94e7c6cf085c
Hannes Schnaitter <git@schnaitter.info>
2026-05-12 16:36:59 +0200

    test: add test for parsing the scaffold.txt file

---
+++ test_scaffold.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S uv run pytest
+from pathlib import Path
+
+import pytest
+
+from scaffold import parse_path, parse_scaffold
+
+
+def test_parse_scaffold_with_filenames():
+    test_file = ["comment\n", "+ add\n", "- subtract\n"]
+    assert parse_scaffold(test_file) == {"create": [("file", Path("add"))], "delete": [("file", Path("subtract"))]}
+
+
+def test_parse_scaffold_disallows_navigation_to_parent():
+    test_file = ["comment\n", "+ ..\n", "- subtract\n"]
+    with pytest.raises(ValueError):
+        parse_scaffold(test_file)
+
+
+def test_parse_path_as_file():
+    assert ("file", Path("test/test.py")) == parse_path("test/test.py")
+    assert ("file", Path("test")) == parse_path("test")
+
+
+def test_parse_path_as_directory():
+    assert ("dir", Path("test/test/")) == parse_path("test/test/")
+    assert ("dir", Path("test/")) == parse_path("test/")
